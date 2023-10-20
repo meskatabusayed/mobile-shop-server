fbf56da84e0133f6c6d21125808af935127c7188
meskatabusayed <meskatabusayed@gmail.com>
2023-10-20 14:43:58 +0600

    Update API documentation

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ run().catch(console.dir);
 
 
 app.get('/' , (req , res) => {
-    res.send('Meskat Mohammad Assignment 10 is Running ')
+    res.send('Meskat Mohammad Abusayed Assignment 10 is Running ')
 })
 
 // data get , from datas.json
